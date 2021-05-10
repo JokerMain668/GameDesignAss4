ec81b241392a57c0f297f8c0a7c644dc66c06757
Theo <62055084+theoho-png@users.noreply.github.com>
2021-05-11 02:40:43 +1000

    Add Theo level

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField]
+    GameObject door, plate;
+
+    bool isOpened = false;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider col)
+    {
+        if (!isOpened)
+        {
+            isOpened = true;
+            door.transform.position += new Vector3(0,0,5);
+            plate.transform.position -= new Vector3(0,.05F,0);
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (isOpened)
+        {
+            isOpened = false;
+            door.transform.position -= new Vector3(0,0,5);
+            plate.transform.position += new Vector3(0,.05F,0);
+        }
+    }
+}
